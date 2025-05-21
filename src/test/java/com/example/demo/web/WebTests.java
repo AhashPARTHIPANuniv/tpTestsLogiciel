fdda37c42660dc415c739da5db58691b81cd4292
AhashPARTHIPANuniv <michel.parthipan@edu.univ-paris13.fr>
2025-05-21 10:57:36 +0200

    juste pour commit qqchose

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -20,6 +20,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+// rien juste pour le commit
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class WebTests {
